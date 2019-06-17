e8968d6520bafa75cf3db8e57a33c42bc2bed8ab
selincano <selin.cano@yahoo.com>
2019-06-17 15:19:14 +0200

    article

--- src/main/java/repository/IArticleRepository.java
+++ src/main/java/repository/IArticleRepository.java
@@ -3,10 +3,13 @@ package repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import metier.*;
 
 public interface IArticleRepository extends JpaRepository<Article, Integer>{
-
+	@Query("select s from Article s left join fetch s.noArticle where s.id=:id")
+	public Optional<Article> findByIdWithNoArticle(@Param("id") Integer id);
 
 }
--- src/main/java/services/ArticleService.java
+++ src/main/java/services/ArticleService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import metier.Adherent;
 import metier.Article;
 import repository.IArticleRepository;
 
@@ -20,24 +21,11 @@ public class ArticleService {
 		iArticleRepository.save(article);
 		return true;
 	}
-
-
-//	public void addArticle(Article article) {
-//		Optional<Article> opt = iArticleRepository.findByIdWithArticle(article.getNoArticle());
-//		if (opt.isPresent()) {
-//			Article article = new Article();
-//			
-//
-//			articlefilm.setKey(new ArticleFilmKey(opt.get(), filmRepository.findById(film.getId()).get()));
-//			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-//			try {
-//				soldatGrade.setDateAffectation(sdf.parse("10/02/2018"));
-//			} catch (ParseException e) {
-//				
-//				e.printStackTrace();
-//			}
-//			soldatGradeRepository.save(soldatGrade);
-//		}
-//	}
-
+	
+	public void delete(Article article) {
+		Optional<Article> opt = iArticleRepository.findByIdWithNoArticle(article.getNoArticle());
+		if (opt.isPresent()) {
+			iArticleRepository.delete(opt.get());
+		}
+	}
 }
