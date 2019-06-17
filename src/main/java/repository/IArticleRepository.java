f0d52f4627816d1b2d35023c0a64735d045461ed
selincano <selin.cano@yahoo.com>
2019-06-17 15:10:30 +0200

    merging master

--- src/main/java/repository/IArticleRepository.java
+++ src/main/java/repository/IArticleRepository.java
@@ -1,9 +1,12 @@
 package repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import metier.*;
 
 public interface IArticleRepository extends JpaRepository<Article, Integer>{
+
 
 }
--- src/main/java/services/ArticleService.java
+++ src/main/java/services/ArticleService.java
@@ -1,59 +1,39 @@
 package services;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import metier.*;
-import repository.*;
-import dao.*;
+import metier.Article;
+import repository.IArticleRepository;
 
 @Service
 public class ArticleService {
 	@Autowired
 	private IArticleRepository iArticleRepository;
-
-	@Autowired
-	private IFilmRepository iFilmRepository;
 	
-	@Autowired
-	private IAdherentRepository iadherentRepository;
-
-	@Autowired
-	private IArticleIFilmRepository iArticleiFilmRepository;
-
-	
-	public boolean insert(IArticle iarticle) {
-		if (iarticle.getNom() == null || iarticle.getNom().isEmpty()) {
+	public boolean insert(Article article) {
+		if (article.getNoArticle() == null) {
 			return false;
 		}
-		iarticleRepository.save(iarticle);
+		iArticleRepository.save(article);
 		return true;
 	}
 
-	
-	public void delete(IArticle soldat) {
-		Optional<Soldat> opt = soldatRepository.findByIdWithGrades(soldat.getId());
-		if (opt.isPresent()) {
-			soldatRepository.delete(opt.get());
-		}
-	}
 
-//	public void addGrade(Soldat soldat, Grade grade) {
-//		Optional<Soldat> opt = soldatRepository.findByIdWithGrades(soldat.getId());
+//	public void addArticle(Article article) {
+//		Optional<Article> opt = iArticleRepository.findByIdWithArticle(article.getNoArticle());
 //		if (opt.isPresent()) {
-//			SoldatGrade soldatGrade = new SoldatGrade();
+//			Article article = new Article();
+//			
 //
-//			soldatGrade.setKey(new SoldatGradeKey(opt.get(), gradeRepository.findById(grade.getId()).get()));
+//			articlefilm.setKey(new ArticleFilmKey(opt.get(), filmRepository.findById(film.getId()).get()));
 //			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 //			try {
 //				soldatGrade.setDateAffectation(sdf.parse("10/02/2018"));
 //			} catch (ParseException e) {
-//				// TODO Auto-generated catch block
+//				
 //				e.printStackTrace();
 //			}
 //			soldatGradeRepository.save(soldatGrade);
