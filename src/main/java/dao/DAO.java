8b1c6536af74201a03f3f54aa7135c12f2301967
FOSSE <raphael.fosse91@gmail.com>
2019-06-17 10:35:33 +0200

    dao_metier done

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.List;
+
+public interface DAO<T,K> {
+
+	
+	public void insert(T object);
+	public T selectById(K id);
+	public void update(T object);
+	public void delete(T object);
+	public List<T> selectAll();
+}
