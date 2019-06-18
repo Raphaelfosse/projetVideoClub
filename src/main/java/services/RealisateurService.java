c4842345578c65b791dcd090fb96430777de5d1b
FOSSE <raphael.fosse91@gmail.com>
2019-06-18 10:03:20 +0200

    it works !

--- src/main/java/services/RealisateurService.java
+++ src/main/java/services/RealisateurService.java
@@ -1,9 +1,11 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import repository.IRealisateurRepository;
 
+@Service
 public class RealisateurService {
 	@Autowired
 	private IRealisateurRepository realisateurRepository;
